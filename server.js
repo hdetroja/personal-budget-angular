148a60e949fa0f633d7f9b48ac54ffb1c6fe7b3c
Heet Detroja <detrojaheet@gmail.com>
2020-10-06 19:58:16 -0400

    Added CORS

--- server.js
+++ server.js
@@ -1,14 +1,46 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use('/' , express.static('public'));
+app.use(cors());
 
-let monthly_budget = require('./budget.json');
+let monthly_budget = {
+    "myBudget": [
+        {
+    "title": "Eat out",
+    "budget": 25
+},
+{
+    "title": "Rent",
+    "budget": 375
+},
+{
+    "title": "Grocery",
+    "budget": 110
+},
+{
+    "title": "Car Insurance",
+    "budget": 200
+},
+{
+    "title": "Water,Electricity Bills",
+    "budget": 100
+},
+{
+    "title": "Student Loan",
+    "budget": 300
+},
+{
+    "title": "Mobile Plan",
+    "budget": 60
+},
+{
+    "title": "Misc",
+    "budget": 50
+},]
+};
 
-app.get('/hello', (req,res) => {
-    res.send('Hello World!');
-});
 
 app.get('/budget', (req,res) => {
     res.json(monthly_budget);
